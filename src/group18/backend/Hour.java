9835e4a21824f82f5fce1c9451a1ae77924774b1
Nyagz <44738554+nyagz@users.noreply.github.com>
2019-05-16 16:01:34 +0100

    Hour backend class

--- src/group18/backend/Hour.java
+++ src/group18/backend/Hour.java
@@ -1,5 +1,12 @@
 package group18.backend;
 
-public class Hour
-{
+public class Hour{
+    private Time time;
+    private int degrees;
+    private int feelsLike;
+    private int humidity;
+    private Date date;
+    private WeatherType weathertype;
+
+
 }
