dc269c4dfa0af20b3a0b77b9d1ff60f8780e6029
ivanstoyanov <ivanstoqnovsmg@gmail.com>
2019-05-16 14:08:23 +0100

    DailyView

---
+++ src/group18/screens/DayPanel.java
@@ -0,0 +1,12 @@
+package group18.screens;
+
+import javax.swing.*;
+
+public class DayPanel
+{
+    private JPanel main;
+    private JLabel lbDate;
+    private JLabel lbDegrees;
+    private JLabel lbWeatherIcon;
+    private JLabel lbCallendarIcon;
+}
