c79f0b68949f21adac0ba524a8320a4e869ea27f
JaySutcliffe <50626754+JaySutcliffe@users.noreply.github.com>
2019-05-16 13:53:48 +0100

    Settings
    Fleshed out the layout of settings

--- src/group18/screens/Settings.java
+++ src/group18/screens/Settings.java
@@ -3,11 +3,33 @@ package group18.screens;
 import javax.swing.*;
 
 public class Settings {
-    private JPanel panel1;
-    private JPanel panel2;
+    private JPanel settingsPanel;
+    private JPanel mainPanel;
     private JButton backButton;
-    private JRadioButton radioButton1;
-    private JRadioButton radioButton2;
+    private JRadioButton celciusRadioButton;
+    private JRadioButton fahrenheitRadioButton;
+    private JSlider coldSlider;
+    private JSlider hotSlider;
+    private JPanel topPanel;
+    private JPanel unitHolderPanel;
+    private JPanel unitLayoutPanel;
+    private JPanel thermometerIcon;
+    private JLabel UnitLabel;
+    private JPanel notificationHolderPanel;
+    private JPanel notificationLayoutPanel;
+    private JPanel notificationIcon;
+    private JLabel notificationLabel;
+    private JRadioButton onRadioButton;
+    private JRadioButton offRadioButton;
+    private JLabel coldLabel;
+    private JLabel hotLabel;
+    private JLabel belowLabel;
+    private JLabel aboveLabel;
+    private JPanel locationHolderPanel;
+    private JButton selectLocationButton;
+    private JPanel locationLayoutPanel;
+    private JLabel locationLabel;
+    private JLabel selectionLabel;
 
     private void createUIComponents() {
         // TODO: place custom component creation code here
