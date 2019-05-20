f3ca8d8bc5235725fee604eb94ecd6c8c5b22a85
JaySutcliffe <50626754+JaySutcliffe@users.noreply.github.com>
2019-05-20 21:05:17 +0100

    Home
    Home stuff

--- src/group18/models/HomeModel.java
+++ src/group18/models/HomeModel.java
@@ -1,17 +1,29 @@
 package group18.models;
 
-import com.github.dvdme.ForecastIOLib.FIOCurrently;
-import com.github.dvdme.ForecastIOLib.ForecastIO;
+import group18.backend.Day;
 import group18.screens.HomeView;
+
+import javax.swing.*;
 
 public class HomeModel {
     public HomeView homeView;
 
 
-    public HomeModel (HomeView homeView, FIOCurrently currently) {
+    public HomeModel (SettingsModel settingModel, HomeView homeView) {
         this.homeView = homeView;
+        // Add Day today as a parameter
 
-        
+        /*
+        // Setting up the average temperature
+        double averageTemp = 0.5*(today.getMaxTemperature()+today.getMinTemperature());
+        // Using function in settings model to convert to string
+        String tempStr = settingModel.getInUnits(averageTemp);
+        homeView.actualTemperature.setText(tempStr);
+        // Our API does not provide this so we are just setting it to the current
+        // temperature for now
+        homeView.feelsLike.setText("Feels like " + tempStr);
+        */
+
 
     }
 }
