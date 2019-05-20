f8a3279f25e6d63540f56bf70ee3a58475809a2e
munetomo <munetomo.t@btinternet.com>
2019-05-20 21:48:01 +0100

    Munetomo's edit

--- src/group18/controllers/HourlyController.java
+++ src/group18/controllers/HourlyController.java
@@ -1,39 +1,81 @@
 package group18.controllers;
 
 import group18.Main;
+import group18.backend.Day;
+import group18.backend.Hour;
 import group18.models.HourlyModel;
 import group18.screens.DailyView;
+import group18.screens.DayPanel;
+import group18.screens.HourlyPanels;
 import group18.screens.HourlyView;
 import group18.api.WeatherAPI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.List;
 
 public class HourlyController {
     private HourlyView view;
     private HourlyModel model;
 
-    //public HourlyController(HourlyView view) {
-    //    this.view = view;
-    //    initModel();
-    //    view.addHourlyButtonListener(a ->
-    //    {
-    //        Main.app.setViewSettings();
-    //    });
-    //    view.addHourlyForecast(model,getHourList());
-    //}
-//
-    //private void initModel(){
-    //    model = new HourlyModel();
-    //    model.loadHourlyModel();
-    //}
 
-    //public DailyController(DailyView view) {
-    //    this.view = view;
-    //    initModel();
-    //    view.addDailyButtonListener(a ->
-    //    {
-    //        Main.app.setViewSettings();
-    //    });
-    //    view.addDailyForecast(model.getDayList());
-//
-    //}
+    public HourlyController(HourlyView view) {
+        this.view = view;
+        initModel();
+        view.addHourlyButtonListener(a ->
+        {
+            Main.app.setViewSettings();
+        });
+        addHourlyForecast(model.getHourlyList());
+//        view.addHourlyForecast(model.getHourList());
+    }
+
+    private void initModel(){
+        model = new HourlyModel();
+        model.loadHourlyForecast();
+    }
+
+    public void addHourlyForecast(List<Hour> HourList)
+    {
+        GridBagLayout layout = new GridBagLayout();
+        view.spHourList.setLayout(layout);
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = 1;
+        int y = 0;
+
+
+        for (Hour hour : HourList)
+        {
+            HourlyPanels dayPanel = new HourlyPanels();
+            constraints.gridy = y++;
+            dayPanel.main.setBorder(BorderFactory.createLineBorder(Color.black));
+            view.spHourList.add(dayPanel.main, constraints);
+            dayPanel.main.addMouseListener(new MouseAdapter()
+            {
+                @Override
+                public void mouseClicked(MouseEvent e)
+                {
+                    super.mouseClicked(e);
+
+//                    TODO: OPEN THE Hourly Screen
+                    System.out.println("Opening for day: " + hour.getDayOfMonth());
+
+                }
+            });
+
+//            TODO
+//            dayPanel.lbCallendarIcon.setIcon();
+//            dayPanel.lbWeatherIcon.setIcon();
+            dayPanel.lbDate.setText(Integer.toString(hour.getHour()));
+            dayPanel.lbDegrees.setText("" + (hour.getTemperature()));
+
+            dayPanel.main.setVisible(true);
+
+
+        }
+    }
 
 }
